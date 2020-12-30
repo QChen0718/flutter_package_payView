6f0c82782cb895c03a2a41d93a77cb35fdf5aa53
white <chenqing0718@163.com>
2020-12-30 11:00:35 +0800

    dart 插件

---
+++ lib/flutter_package_payview.dart
@@ -0,0 +1,21 @@
+library flutter_package_payview;
+
+import 'package:flutter/material.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+class PayView extends StatefulWidget{
+  @override
+  _PayViewState createState() => _PayViewState();
+}
+class _PayViewState extends State<PayView>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
